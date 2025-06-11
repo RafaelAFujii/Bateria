b4b05fa89c00fdb3a9875115dacbea7c50890136
Rafael Fujii <akira-rafael@outlook.com.br>
2025-06-11 11:11:39 -0300

    Inicio do desenvolvimento em Javascript
    Primeiro parte do desenvolvimento, onde o sistema identifica o "Click" no botão

--- index.js
+++ index.js
@@ -1,4 +1,43 @@
 // Função para detectar o click do mouse.
+const drumButtons = document.querySelectorAll('.drum'); //Pequisa no HTML todos os elementos com a classe "drum".
+drumButtons.forEach(button => { //Verifica todos os botões encontrados 
+    button.addEventListener('click', () => { //Cria um evento ao clicar no botão
+        let buttonLetter = this.innerHTML 
+        console.log("A letra que foi pressionada é: " + buttonLetter);
+        switch (buttonLetter) { 
+            case 'a':
+                console.log("Você pressionou a tecla A");
+                break;
+            
+            case 's':
+                console.log("Você pressionou a tecla S");
+                break;
+
+            case 'd':
+                console.log("Você pressionou a tecla D");
+                break;
+
+            case 'g':
+                console.log("Você pressionou a tecla G");
+                break;
+
+            case 'j':
+                console.log("Você pressionou a tecla J");
+                break;
+
+            case 'k':
+                console.log("Você pressionou a tecla K");
+                break;
+
+            case 'l':
+                console.log("Você pressionou a tecla L");
+                break;
+
+            default:
+                console.log("Tecla não reconhecida");
+        }
+    })
+});
 
 // Função para detectar o pressionamento da tecla.
 
